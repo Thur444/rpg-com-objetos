8c1c44a0a1f201a69d63eefe56a7f10f7de43870
Thur444 <arthurghp20@gmail.com>
2026-08-13 09:09:29 -0300

    A batalha

--- app.js
+++ app.js
@@ -1,44 +1,49 @@
-const guerreiro = {
-    nome: "Thorin",
-    vida: 60,
-    ataque: 20,
+   class Personagem {
+       constructor (nome, vida, ataque){
+        this.nome = nome,
+        this.vida = vida,
+        this.ataque = ataque
+       }
 
-causaDano: function(personagem){
-personagem.recebeDano(this.ataque)
- },
+       causaDano(personagem){
+        personagem.recebeDano(this.ataque)
+       }
+       recebeDano(quantidade){
+        this.vida = this.vida - quantidade 
 
- recebeDano: function(quantidade){
-this.vida = this.vida - quantidade
-    }
-}
+        if(this.vida <= 0){
+            this.vida = "eliminado"
+        }
+       }
+   }
 
-const mago = {
-nome: "Gandalfe",
-vida: 60,
-ataque: 35,
+   const druida = new Personagem ("Kode", 80, 25)
+  
+   
 
-recebeDano: function(quantidade){
-this.vida = this.vida - quantidade
- },
+const guerreiro = new Personagem("Thorin", 60, 20)
 
- causaDano: function(personagem){
-personagem.recebeDano(this.ataque)
- }
-}
+const mago = new Personagem("Gandalfe", 60, 35)
 
-const arqueiro = {
-nome: "Legolas",
-vida: 80,
-ataque: 25,
+const arqueiro = new Personagem("Legolas", 80, 25)
 
-recebeDano: function(quantidade){
-this.vida = this.vida - quantidade
- },
+const cowboy = new Personagem("Woody", 95, 40)
 
- causaDano: function(personagem){
-personagem.recebeDano(this.ataque)
- }
-}
+const robo = new Personagem("Buzz Lightyear", 90, 50)
+
+const imperador = new Personagem("Zurg", 120, 80)
 
 guerreiro.causaDano(mago)
-console.log(mago.vida)
+arqueiro.causaDano(mago)
+guerreiro.causaDano(mago)
+robo.causaDano(imperador)
+imperador.causaDano(robo)
+cowboy.causaDano(imperador)
+imperador.causaDano(cowboy)
+robo.causaDano(imperador)
+
+
+console.log(cowboy.vida)
+console.log(imperador.vida)
+console.log(robo.vida)
+console.log("Woody e Buzz lightyear derrotaram Zurg")
